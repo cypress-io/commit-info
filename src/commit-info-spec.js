3dd15b7f505a3f3430c5269c32b28e5cb7bba5a3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-27 21:38:06 -0400

    initial code

---
+++ src/commit-info-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const commitInfo = require('.')
+
+describe('commit-info', () => {
+  it('write this test', () => {
+    console.assert(commitInfo, 'should export something')
+  })
+})
